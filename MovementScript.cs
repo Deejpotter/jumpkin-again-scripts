6da820c12de4dc528e277115ecf74b143b0614d6
Deej <deejpotter@gmail.com>
2021-08-09 18:13:32 +1000

    Added everything else.

---
+++ MovementScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    CharacterController characterController;
+    Animator ani;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 oldPos = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            if (transform.position != oldPos)
+            {
+                ani.SetBool("Walking", true);
+            }
+            else
+            {
+                ani.SetBool("Walking", false);
+            }
+
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+
+            oldPos = transform.position;
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
